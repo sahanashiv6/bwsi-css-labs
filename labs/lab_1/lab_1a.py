f89d936124ade0d5f1bebee0c62a8a54ad692041
Sahana Shivkumar <sahana.shivkumar@outlook.com>
2026-02-25 20:01:27 -0800

    Added extra line to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Sahana Shivkumar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print(f"{name}, you will do amazing!")
+          
 if __name__ == "__main__":
     main()
